9578cc06d1ea83ac09db59286ab267d03bf4f0c4
surjeetkumarkharwar <140419537+surjeetkumarkharwar@users.noreply.github.com>
2023-07-25 20:17:38 +0530

    Add files via upload

---
+++ concpet.js
@@ -0,0 +1,25 @@
+var num1 =5;
+var num2 =7;
+if (num1==num2)
+{
+    console.log( "Both numbers are equal");  // Both the statements will execute as both conditions
+}
+else if(num2>num1)
+{
+    console.log( "num2 greater then to num1");
+}
+else if (num2<num1)
+{
+    console.log ("num1 is less than num2" );
+}
+else{
+    console.log("both number are not equal")
+}
+switch (num1+num2)
+{
+    case  5:
+        console.log (5)
+         break
+    case   12:
+        console.log (12)
+}
